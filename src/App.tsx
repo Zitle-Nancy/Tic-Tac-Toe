8735ef49442b13b620975899633b206054641623
Nancy Zitle Juárez <nancy-zitle@Nancys-MacBook-Pro.local>
2023-05-16 18:25:23 -0700

    feat(App): modify App and index files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const Square = () => {
+  const [value, setValue] = useState<null | string>(null);
+
+  const handleClick = () => setValue("X");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+};
+const Board = () => {
+  return (
+    <div className="container">
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Board;
